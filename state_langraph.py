fb23ae8b49606b3251ccae6e0ff3471b81eadab6
jdelpego <jdelpego@gmail.com>
2025-12-26 08:21:05 -0800

    Successful memory store  across threads/agent invokations
    Not successful persistence across script runs (checkpointer uses RAM)

--- state_langraph.py
+++ state_langraph.py
@@ -1,18 +1,28 @@
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
+from langchain_core.runnables import RunnableConfig
+from langgraph.checkpoint.memory import InMemorySaver
 
 class State(TypedDict):
     count: int
 
 def add_1(state: State) -> State:
     """Adds 1 to the current count in state"""
-    return {"count": state["count"] + 1}
-    
+    return {"count": state["count"] + 1} 
 
 builder = StateGraph(State)
 builder.add_node("add_1", add_1)
 builder.add_edge(START, "add_1")
 builder.add_edge("add_1", END)
-agent = builder.compile()
-response = agent.invoke({"count": 0})
-print(response)
+
+checkpointer = InMemorySaver()
+
+agent = builder.compile(checkpointer=checkpointer)
+
+config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+
+print(agent.invoke({"count": 0}, config))
+print(agent.invoke({}, config))
+print(agent.invoke({}, config))
+
+
